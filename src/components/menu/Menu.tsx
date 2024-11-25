8653a25a94889004c38c691f97feffb0cec531c2
VictorLuigi27 <victor.diaz@oclock.school>
2024-11-25 13:06:14 +0100

    Glow menu text

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -5,8 +5,9 @@ export default function Menu() {
       <div className="bg-black min-h-screen no-scroll">
           {/* Le reste du contenu */}
           <div className="flex items-center justify-between relative">
+
               <div 
-                className="relative"
+                className="relative menu-img"
                 style={{ position: 'relative', top: '44.5vh', left: '20vw' }}
               >
                 <Link to='/experiences'>
@@ -19,12 +20,10 @@ export default function Menu() {
                         Formations
                     </p>
                 </Link>
-                  
-                  
               </div>
         
                 <Link to='/projets'>
-                    <p className="text-white text-4xl font-almendra" style={{ position: 'relative', top: '44.5vh', right: '24vw' }}>
+                    <p className="text-white text-4xl font-almendra menu-img" style={{ position: 'relative', top: '44.5vh', right: '24vw' }}>
                         Projets
                     </p>
                 </Link>
@@ -35,13 +34,13 @@ export default function Menu() {
           <div className="relative">
 
             <Link to='/competences'>
-                <p className="text-white text-4xl font-almendra" style={{ position: 'relative', left: '44vw' }}>
+                <p className="text-white text-4xl font-almendra menu-img" style={{ position: 'relative', left: '44vw' }}>
                     Compétences
                 </p>
             </Link>
               
             <Link to='/contact'>
-                <p className="text-white text-4xl font-almendra" style={{ position: 'relative', top: '70vh', left: '46.2vw' }}>
+                <p className="text-white text-4xl font-almendra menu-img" style={{ position: 'relative', top: '70vh', left: '46.2vw' }}>
                     Contact
                 </p>
             </Link>
